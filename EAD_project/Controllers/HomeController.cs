5b3a5f324cb19ef103e946565e0ac51438659b22
kazam <syedkazamraza20@gmail.com>
2021-09-06 13:54:21 +0500

    All files added

---
+++ EAD_project/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using EAD_Project.Models;
+
+namespace EAD_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseEntities db = new DatabaseEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult userlogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult confirmLogin()
+        {
+            user u = new user();
+            string nm = Request["username"];
+            string pass = Request["password"];
+            var obj = db.users.Where(query => query.username.Equals(nm) && query.password.Equals(pass)).SingleOrDefault();
+
+            if(obj != null)
+            {
+                FormsAuthentication.SetAuthCookie(nm, false);
+                return RedirectToAction("index");
+
+            }
+            return View();
+        }
+
+        public ActionResult signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveCredientials()
+        {
+            string uname = Request["Name"];
+            string pass = Request["password"];
+            user u = new user();
+            u.username = uname;
+            u.password = pass;
+            u.type = "customer";
+            db.users.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("createaccount");
+        }
+
+        public ActionResult createaccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ EAD_project/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EAD_Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EAD_Project")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("432b001c-5bb1-4848-85a0-44ae649a2c52")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
